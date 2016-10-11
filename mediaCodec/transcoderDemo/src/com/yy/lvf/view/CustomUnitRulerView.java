e05df27499601581f3ecdb14f53cd14a7cee8e2d
duowanSir <529421271@qq.com>
2016-10-11 23:39:12 +0800

    可自定义最小单元和倍数的尺子组件（ruler）

--- mediaCodec/transcoderDemo/src/com/yy/lvf/view/CustomUnitRulerView.java
+++ mediaCodec/transcoderDemo/src/com/yy/lvf/view/CustomUnitRulerView.java
@@ -10,6 +10,7 @@ import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.util.TypedValue;
+import android.view.MotionEvent;
 import android.view.View;
 
 /**
@@ -19,15 +20,18 @@ import android.view.View;
  * view的事件传递
  * canvas和paint的运用和matrix运用
  */
-public class VideoBitRateRulerView extends View {
+public class CustomUnitRulerView extends View {
 	private final static boolean VERBOSE = false;
-	private final static String TAG = VideoBitRateRulerView.class.getSimpleName();
+	private final static String TAG = CustomUnitRulerView.class.getSimpleName();
 	private int mMinUnit;// 最小单位
 	private int mMultiple;// 最大刻度和最小刻度之间的倍数;
 	private int mMinValue;// 最大值
 	private int mMaxValue;// 最小值
+	
+	private float dp1;
 
 	private float mIndicatorHeight;
+	private int mIndicatorColor;
 	private float mMinUnitWidth;
 	private float mMinUnitLineWidth;
 	private float mMinUnitLineHeight;
@@ -42,25 +46,33 @@ public class VideoBitRateRulerView extends View {
 	private Paint mMidUnitLinePaint;
 	private Paint mMaxUnitLinePaint;
 	private Paint mIndicatorPaint;
+	
+	private boolean mNeedRounding = true;
 
-	public VideoBitRateRulerView(Context context) {
+	public CustomUnitRulerView(Context context) {
 		super(context);
 		init(context, null);
 	}
 
-	public VideoBitRateRulerView(Context context, AttributeSet attrs) {
+	public CustomUnitRulerView(Context context, AttributeSet attrs) {
 		super(context, attrs);
 		init(context, attrs);
 	}
 
-	public VideoBitRateRulerView(Context context, AttributeSet attrs, int defStyleAttr) {
+	public CustomUnitRulerView(Context context, AttributeSet attrs, int defStyleAttr) {
 		super(context, attrs, defStyleAttr);
 		init(context, attrs);
 	}
 
 	private void init(Context context, AttributeSet attrs) {
+	    	setClickable(true);
 		TypedArray a = null;
-		int dp1 = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 1, context.getResources().getDisplayMetrics());
+		dp1 = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 1, context.getResources().getDisplayMetrics());
+		mMinUnit = 1;
+		mMinValue = 0;
+		mMaxValue = 100;
+		mMultiple = 10;
+		
 		mIndicatorHeight = dp1 * 10;
 		mMinUnitWidth = dp1 * 5;
 		mMinUnitLineWidth = mMidUnitLineWidth = dp1;
@@ -68,48 +80,125 @@ public class VideoBitRateRulerView extends View {
 		mMinUnitLineHeight = dp1 * 2;
 		mMidUnitLineHeight = mMinUnitLineHeight * 2;
 		mMaxUnitLineHeight = mMidUnitLineHeight + mMinUnitLineHeight;
+		mIndicatorColor = Color.BLUE;
 		mMinUnitLineColor = Color.GRAY;
 		mMidUnitLineColor = Color.DKGRAY;
 		mMaxUnitLineColor = Color.BLACK;
 
 		if (attrs != null) {
 			try {
-				a = context.obtainStyledAttributes(attrs, R.styleable.VideoBitRateRulerView, 0, 0);
-				mIndicatorHeight = a.getDimension(R.styleable.VideoBitRateRulerView_indicator_height, mIndicatorHeight);
-				mMinUnitWidth = a.getDimension(R.styleable.VideoBitRateRulerView_min_unit_width, mMinUnitWidth);
-				mMinUnitLineWidth = a.getDimension(R.styleable.VideoBitRateRulerView_min_unit_line_witdh, dp1);
-				mMinUnitLineHeight = a.getDimension(R.styleable.VideoBitRateRulerView_min_unit_line_height, mMinUnitLineHeight);
-				mMidUnitLineWidth = a.getDimension(R.styleable.VideoBitRateRulerView_mid_unit_line_witdh, dp1);
-				mMidUnitLineHeight = a.getDimension(R.styleable.VideoBitRateRulerView_mid_unit_line_height, mMidUnitLineHeight);
-				mMaxUnitLineWidth = a.getDimension(R.styleable.VideoBitRateRulerView_max_unit_line_witdh, mMaxUnitLineWidth);
-				mMaxUnitLineHeight = a.getDimension(R.styleable.VideoBitRateRulerView_max_unit_line_height, mMaxUnitLineHeight);
-				mMinUnitLineColor = a.getColor(R.styleable.VideoBitRateRulerView_min_unit_line_color, mMinUnitLineColor);
-				mMidUnitLineColor = a.getColor(R.styleable.VideoBitRateRulerView_mid_unit_line_color, mMidUnitLineColor);
-				mMaxUnitLineColor = a.getColor(R.styleable.VideoBitRateRulerView_max_unit_line_color, mMaxUnitLineColor);
+				a = context.obtainStyledAttributes(attrs, R.styleable.CustomUnitRulerView, 0, 0);
+				mMinUnit = a.getInt(R.styleable.CustomUnitRulerView_min_unit, mMinUnit);
+				mMinValue = a.getInt(R.styleable.CustomUnitRulerView_min_value, mMinValue);
+				mMaxValue = a.getInt(R.styleable.CustomUnitRulerView_max_value, mMaxValue);
+				mMultiple = a.getInt(R.styleable.CustomUnitRulerView_multiple, mMultiple);
+				
+				mIndicatorHeight = a.getDimension(R.styleable.CustomUnitRulerView_indicator_height, mIndicatorHeight);
+				mIndicatorColor = a.getColor(R.styleable.CustomUnitRulerView_indicator_color, mIndicatorColor);
+				mMinUnitWidth = a.getDimension(R.styleable.CustomUnitRulerView_min_unit_width, mMinUnitWidth);
+				mMinUnitLineWidth = a.getDimension(R.styleable.CustomUnitRulerView_min_unit_line_witdh, dp1);
+				mMinUnitLineHeight = a.getDimension(R.styleable.CustomUnitRulerView_min_unit_line_height, mMinUnitLineHeight);
+				mMidUnitLineWidth = a.getDimension(R.styleable.CustomUnitRulerView_mid_unit_line_witdh, dp1);
+				mMidUnitLineHeight = a.getDimension(R.styleable.CustomUnitRulerView_mid_unit_line_height, mMidUnitLineHeight);
+				mMaxUnitLineWidth = a.getDimension(R.styleable.CustomUnitRulerView_max_unit_line_witdh, mMaxUnitLineWidth);
+				mMaxUnitLineHeight = a.getDimension(R.styleable.CustomUnitRulerView_max_unit_line_height, mMaxUnitLineHeight);
+				mMinUnitLineColor = a.getColor(R.styleable.CustomUnitRulerView_min_unit_line_color, mMinUnitLineColor);
+				mMidUnitLineColor = a.getColor(R.styleable.CustomUnitRulerView_mid_unit_line_color, mMidUnitLineColor);
+				mMaxUnitLineColor = a.getColor(R.styleable.CustomUnitRulerView_max_unit_line_color, mMaxUnitLineColor);
 			} finally {
 				a.recycle();
 			}
 		}
+		initPaint();
 	}
 
 	private void initPaint() {
+	    mIndicatorPaint = new Paint();
+	    mIndicatorPaint.setColor(mIndicatorColor);
+	    
+	    mMinUnitLinePaint = new Paint();
+	    mMinUnitLinePaint.setColor(mMinUnitLineColor);
+	    mMinUnitLinePaint.setStrokeWidth(mMinUnitLineWidth);
+	    
+	    mMidUnitLinePaint = new Paint();
+	    mMidUnitLinePaint.setColor(mMidUnitLineColor);
+	    mMidUnitLinePaint.setStrokeWidth(mMidUnitLineWidth);
+	    
+	    mMaxUnitLinePaint = new Paint();
+	    mMaxUnitLinePaint.setColor(mMaxUnitLineColor);
+	    mMaxUnitLinePaint.setStrokeWidth(mMaxUnitLineWidth);
 	}
 
 	@Override
 	public void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
 		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-		setMeasuredDimension(0, 0);
+		setMeasuredDimension(View.MeasureSpec.getSize(widthMeasureSpec)
+			, measureHeight(heightMeasureSpec));
 	}
 
+	private boolean isFirstTime = true;
 	@Override
 	protected void onDraw(Canvas canvas) {
 		super.onDraw(canvas);
+//		if (isFirstTime) {
+//		    drawRulerScale(canvas);
+//		    drawIndicator(canvas);
+//		    isFirstTime = false;
+//		} else {
+//		    int layer = canvas.save();
+//		    if (!mNeedRounding) {
+//			slideCalculate(canvas, mXAxisSlideDistance, mNeedRounding);
+//			drawIndicator(canvas);
+//		    }
+//		    canvas.restoreToCount(layer);
+//		}
+		drawRulerScale(canvas);
+		if (!mNeedRounding) {
+		    slideCalculate(canvas, mXAxisSlideDistance, mNeedRounding);
+		}
+		drawIndicator(canvas);
+		
+	}
+	
+	private float mPreXAxisCoordinate = 0;
+	private float mXAxisSlideDistance = 0;
+	
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+	    	switch (event.getAction()) {
+    		case MotionEvent.ACTION_DOWN:
+    		    mNeedRounding = false;
+    		    break;
+    		case MotionEvent.ACTION_MOVE:
+    		    if (mPreXAxisCoordinate == 0) {
+    			mPreXAxisCoordinate = event.getX();
+    		    } else {
+    			mXAxisSlideDistance = event.getX() - mPreXAxisCoordinate;
+    		    }
+    		    invalidate();
+    		    break;
+    		case MotionEvent.ACTION_UP:
+    		    mNeedRounding = true;
+    		    invalidate();
+    		    break;
+    		default:
+    		    break;
+		}
+	    	return super.onTouchEvent(event);
 	}
 
 	private int measureHeight(int heightMeasureSpec) {
 		int measureMode = View.MeasureSpec.getMode(heightMeasureSpec);
 		int measureSize = View.MeasureSpec.getSize(heightMeasureSpec);
-		return 0;
+		switch (measureMode) {
+		case View.MeasureSpec.UNSPECIFIED:
+		case View.MeasureSpec.EXACTLY:
+		case View.MeasureSpec.AT_MOST:
+		default:
+		    measureSize = (int) (getPaddingBottom() + getPaddingTop() + mIndicatorHeight + mMaxUnitLineHeight + dp1 * 10);
+		    break;
+		}
+		return measureSize;
 	}
 
 	private void drawRulerScale(Canvas canvas) {
@@ -150,7 +239,7 @@ public class VideoBitRateRulerView extends View {
 		}
 	}
 
-	private void drawIndicator(Canvas canvas, boolean needRounding) {// 滑动不取整，滑动完毕取整。
+	private void drawIndicator(Canvas canvas) {// 滑动不取整，滑动完毕取整。
 		float stx, sty, spx, spy;
 		spy = getMeasuredHeight() - mMaxUnitLineHeight;
 		sty = spy - mIndicatorHeight;
