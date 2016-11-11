b00b3ec026062b98b15d04ff8109e9e7c4f0de16
lvfengayy <lvfeng@yy.com>
2016-11-11 18:33:47 +0800

    6%

--- CameraToolkit/gen/com/yy/lvf/camera/R.java
+++ CameraToolkit/gen/com/yy/lvf/camera/R.java
@@ -14,7 +14,7 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int preview_glsv=0x7f060000;
+        public static final int preview_sv=0x7f060000;
         public static final int record_video_btn=0x7f060001;
         public static final int switch_camera_btn=0x7f060003;
         public static final int take_picture_btn=0x7f060002;
--- CameraToolkit/src/com/yy/lvf/camera/CameraActivity.java
+++ CameraToolkit/src/com/yy/lvf/camera/CameraActivity.java
@@ -5,64 +5,198 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.lang.ref.WeakReference;
 import java.util.List;
 
-import javax.microedition.khronos.egl.EGLConfig;
-import javax.microedition.khronos.opengles.GL10;
+import com.yy.lvf.LLog;
+import com.yy.lvf.gles.EglCore;
+import com.yy.lvf.gles.GlUtil;
+import com.yy.lvf.gles.Texture2dProgram;
+import com.yy.lvf.gles.WindowSurface;
 
 import android.app.Activity;
+import android.graphics.SurfaceTexture;
 import android.hardware.Camera;
 import android.hardware.Camera.CameraInfo;
 import android.hardware.Camera.Parameters;
 import android.hardware.Camera.PictureCallback;
 import android.hardware.Camera.Size;
-import android.opengl.GLSurfaceView;
-import android.opengl.GLSurfaceView.Renderer;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.util.Log;
 import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceHolder.Callback;
+import android.view.TextureView.SurfaceTextureListener;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.Button;
 
-public class CameraActivity extends Activity implements OnClickListener {
+public class CameraActivity extends Activity implements OnClickListener, Callback {
+	public static class RenderThread extends Thread implements SurfaceTextureListener {
+		// Used to wait for the thread to start.
+		private Object				mStartLock	= new Object();
+		private boolean				mReady		= false;
+		private RenderHandler		mRenderHandler;
+
+		private EglCore				mEglCore;
+		private WindowSurface		mWindowSurface;
+
+		private SurfaceTexture		mCameraTexture;
+
+		private Texture2dProgram	mTexProgram;
+
+		@Override
+		public void run() {
+			super.run();
+			Looper.prepare();
+			mRenderHandler = new RenderHandler(this);
+			synchronized (mStartLock) {
+				mReady = true;
+				mStartLock.notify();
+			}
+			mEglCore = new EglCore(null, 0);
+
+			Looper.loop();
+
+			releaseGl();
+			mEglCore.release();
+		}
+
+		public void waitUtilReady() throws InterruptedException {
+			synchronized (mStartLock) {
+				while (!mReady) {
+					mStartLock.wait();
+				}
+			}
+		}
+
+		@Override
+		public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+		}
+
+		@Override
+		public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+		}
+
+		@Override
+		public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+			return false;
+		}
+
+		@Override
+		public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+		}
+
+		public RenderHandler getRenderHandler() {
+			return mRenderHandler;
+		}
+
+		private void releaseGl() {
+			GlUtil.checkGlError("releaseGl start");
+
+			if (mWindowSurface != null) {
+				mWindowSurface.release();
+				mWindowSurface = null;
+			}
+			if (mTexProgram != null) {
+				mTexProgram.release();
+				mTexProgram = null;
+			}
+			GlUtil.checkGlError("releaseGl done");
+
+			mEglCore.makeNothingCurrent();
+		}
+	}
+
+	public static class RenderHandler extends Handler {
+		public static final int				MSG_SURFACE_CREATED	= 0;
+		private WeakReference<RenderThread>	mRenderThread;
+
+		public RenderHandler(RenderThread thread) {
+			mRenderThread = new WeakReference<CameraActivity.RenderThread>(thread);
+		}
+
+		@Override
+		public void handleMessage(Message msg) {
+			RenderThread renderThread = mRenderThread.get();
+			if (renderThread == null) {
+				removeCallbacksAndMessages(null);
+				return;
+			}
+			switch (msg.what) {
+			case MSG_SURFACE_CREATED:
+				SurfaceHolder surfaceHolder = (SurfaceHolder) msg.obj;
+				Surface surface = surfaceHolder.getSurface();
+				renderThread.mWindowSurface = new WindowSurface(renderThread.mEglCore, surface, false);
+				renderThread.mWindowSurface.makeCurrent();
+
+				// Create and configure the SurfaceTexture, which will receive frames from the
+				// camera.  We set the textured rect's program to render from it.
+				renderThread.mTexProgram = new Texture2dProgram(Texture2dProgram.ProgramType.TEXTURE_EXT);
+				int textureId = renderThread.mTexProgram.createTextureObject();
+				renderThread.mCameraTexture = new SurfaceTexture(textureId);
+				//	            mRect.setTexture(textureId);
+//				renderThread.mCameraTexture.setOnFrameAvailableListener(this);
+				break;
+			default:
+				break;
+			}
+		}
+	}
+
 	public static final String	TAG	= CameraActivity.class.getSimpleName();
-	private GLSurfaceView		mPreviewGlsv;
+	private SurfaceView			mPreviewSv;
 	private Button				mRecordVideoBtn;
 	private Button				mTakePictureBtn;
 	private Button				mSwitchCameraBtn;
 
 	private int					mCameraId;
-	private Camera				mCamera;
+	private Camera				mCamera;									//camera应该的放在onResume和onPause里面去管理
+
+	private RenderThread		mRenderThread;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.camera_activity);
 
-		mPreviewGlsv = (GLSurfaceView) findViewById(R.id.preview_glsv);
+		mPreviewSv = (SurfaceView) findViewById(R.id.preview_sv);
 		mRecordVideoBtn = (Button) findViewById(R.id.record_video_btn);
 		mTakePictureBtn = (Button) findViewById(R.id.take_picture_btn);
 		mSwitchCameraBtn = (Button) findViewById(R.id.switch_camera_btn);
 		mRecordVideoBtn.setOnClickListener(this);
 		mTakePictureBtn.setOnClickListener(this);
 		mSwitchCameraBtn.setOnClickListener(this);
-		mPreviewGlsv.setRenderer(new Renderer() {
+		SurfaceHolder surfaceHolder = mPreviewSv.getHolder();
+		LLog.d(TAG, surfaceHolder + "");
+		surfaceHolder.addCallback(this);
+	}
 
-			@Override
-			public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-			}
+	@Override
+	public void surfaceCreated(SurfaceHolder holder) {
+		LLog.d(TAG, "surfaceCreated(" + holder + ", " + Thread.currentThread() + ")");
+		try {
+			mCamera.setPreviewDisplay(mPreviewSv.getHolder());
+			mCamera.startPreview();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-			@Override
-			public void onSurfaceChanged(GL10 gl, int width, int height) {
-			}
+	@Override
+	public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+		LLog.d(TAG, "surfaceChanged(" + holder + ", " + format + ", " + width + ", " + height + ")");
+	}
 
-			@Override
-			public void onDrawFrame(GL10 gl) {
-			}
-		});
+	@Override
+	public void surfaceDestroyed(SurfaceHolder holder) {
+		LLog.d(TAG, "surfaceDestroyed(" + holder + ")");
 	}
 
 	@Override
@@ -70,7 +204,14 @@ public class CameraActivity extends Activity implements OnClickListener {
 		super.onResume();
 		mCamera = getCameraInstance(mCameraId);
 		setPreviewSize();
-		
+
+		mRenderThread = new RenderThread();
+		mRenderThread.start();
+		try {
+			mRenderThread.waitUtilReady();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
@@ -161,10 +302,10 @@ public class CameraActivity extends Activity implements OnClickListener {
 				height = size.width;
 			}
 			if (width == getResources().getDisplayMetrics().widthPixels) {
-				LayoutParams previewLp = mPreviewGlsv.getLayoutParams();
+				LayoutParams previewLp = mPreviewSv.getLayoutParams();
 				previewLp.width = width;
 				previewLp.height = height;
-				mPreviewGlsv.setLayoutParams(previewLp);
+				mPreviewSv.setLayoutParams(previewLp);
 				params.setPreviewSize(size.width, size.height);
 				mCamera.setParameters(params);
 				break;
