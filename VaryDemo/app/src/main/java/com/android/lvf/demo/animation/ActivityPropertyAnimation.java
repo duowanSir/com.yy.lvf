fbc315091b4eecfe1ad56eae15982d8e652bb51d
duowanSir <lvfeng0224@gmail.com>
2016-12-16 00:17:18 +0800

    animation

---
+++ VaryDemo/app/src/main/java/com/android/lvf/demo/animation/ActivityPropertyAnimation.java
@@ -0,0 +1,20 @@
+package com.android.lvf.demo.animation;
+
+
+import android.animation.ObjectAnimator;
+import android.app.Activity;
+import android.view.View;
+
+/**
+ * Created by 烽 on 2016/12/15.
+ */
+
+public class ActivityPropertyAnimation extends Activity {
+    private View mSrcV;
+    private View mDstV;
+
+    private void objectAnimation() {
+        ObjectAnimator objectAnimator = new ObjectAnimator();
+//        objectAnimator.
+    }
+}
